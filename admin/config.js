0131d5656dacbe78cf7d2c2411dbc2965b5f943a
Ineffablebbal <Ineffablebbal@gmail.com>
2025-06-22 17:01:04 +0500

    Delete admin directory

--- admin/config.js
+++
@@ -1,9 +0,0 @@
-const ADMIN_CREDENTIALS = {
-    username: 'admin',
-    password: 'admin123'
-  };
-  
-  const API_ROUTES = {
-    products: '../data/products.json',
-    slider: '../data/slider.json'
-  };
